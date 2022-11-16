f1f0f91df375cad9d6f93e7fa376de550be10034
Vereshchagin_Mikhail <farasers@mail.ru>
2022-11-17 00:19:43 +0300

    ДЗ Исключения. №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+        Data data = new Data ("01234567891234567891","Passworд","Passworд");
+
     }
 }
